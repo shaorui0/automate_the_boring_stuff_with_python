a1b400d6cc339d20c53a839b2a8906799bfc50b4
shaorui <sr1054461216@163.com>
2019-06-01 21:33:13 +0800

    chapter11 project

--- chapter_11_downloading All xkcd comics/chapter_11_downloading_All_xkcd_comics.py
+++ chapter_11_downloading All xkcd comics/chapter_11_downloading_All_xkcd_comics.py
@@ -5,3 +5,8 @@
 
 # 打开漫画页，找到第一页（<）,从前往后遍历，每一页的imag解析出来，保存到folder，点击“下一页<li>里面” ，循环，直到无法向后（‘#’）
 # 组织成文件后，zip压缩，通过rss发送给kindle？
+import downloadXkcd
+
+if __name__ == '__main__':
+    url = 'https://xkcd.com/'
+    downloadXkcd.downloadPageContent(url)
--- chapter_11_downloading All xkcd comics/downloadXkcd.py
+++ chapter_11_downloading All xkcd comics/downloadXkcd.py
@@ -1,2 +1,39 @@
 #! python3
 # downloadXkcd.py - Downloads every single XKCD comic.
+
+import requests,bs4,os
+
+def downloadPageContent(url):
+    os.makedirs('xkcd', exist_ok = True)
+    print("download start!")
+    while not url.endswith('#'):
+        # TODO:download page
+        print("Download the page %s" % url)
+        res = requests.get(url)
+        res.raise_for_status()
+
+        noStarchSoup = bs4.BeautifulSoup(res.text)
+        # TODO:find url of the comic image
+            # <div id='comic'>
+            #    <img src='//img_url'>
+        comicElem = noStarchSoup.select('#comic img')
+        if comicElem == []:
+            print("couldn't find comic imag")
+        else:
+            comicUrl = 'http:'+ comicElem[0].get('src')
+            # TODO:download image
+            print('Downloading image %s...' % (comicUrl))
+            res = requests.get(comicUrl)
+            res.raise_for_status()
+
+            # TODO:store img in 'xkcd'
+            imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+            # TODO:get the prev buttom
+            prevLink = noStarchSoup.select('a[rel="prev"]')[0]
+            url = 'http://xkcd.com' + prevLink.get('href')
+
+    print("download done!")
+
